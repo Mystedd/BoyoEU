6808c55256744a54e8a442e87bd9f85f01fec5d4
Myst3d <kianbartley07@gmail.com>
2023-01-23 16:39:11 +0000

    Initial commit

---
+++ src/main/java/boyoeu/boyoeu/BoyoEU.java
@@ -0,0 +1,17 @@
+package boyoeu.boyoeu;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BoyoEU extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("BoyoEU plugin loaded");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info("BoyoEU plugin unloaded");
+    }
+}
